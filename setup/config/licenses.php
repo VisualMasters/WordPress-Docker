eff6d9687ad4d46b3f64067832472c758ca922d2
jstreuper <justin@visualmasters.nl>
2020-12-01 09:44:29 +0100

    Removed ACF filter

--- setup/config/licenses.php
+++ setup/config/licenses.php
@@ -15,21 +15,3 @@
     define( 'ACF_PRO_LICENSE', getenv( 'LICENSE_ACF' ) ?: '' );
     define( 'WP_ROCKET_KEY', getenv( 'LICENSE_WPROCKET' ) ?: '' );
     define( 'WP_ROCKET_EMAIL', getenv( 'LICENSE_WPROCKET_EMAIL' ) ?: '' );
-
-    // ==========================
-	// Add filter for ACF license
-	// ==========================
-    add_filter( 'pre_option_acf_pro_license', function( $pre ) {
-        
-        if ( ! defined( 'ACF_PRO_LICENSE' ) || empty( ACF_PRO_LICENSE ) ) {
-            return $pre;
-        }
-        
-        $data = array(
-            'key' => ACF_PRO_LICENSE,
-            'url' => home_url(),
-        );
-
-        return base64_encode( serialize( $data ) );
-
-    }, 10, 1 );
