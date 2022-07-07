82c6b2ef19c744d13fd84df162f573aa89578e40
jstreuper <justin@burovoordeboeg.nl>
2022-07-07 16:10:39 +0200

    Added extra license keys

--- setup/config/config.php
+++ setup/config/config.php
@@ -78,10 +78,12 @@
     /**
 	 * Define licenses located on premise
 	 */
+    define( 'ACF_PRO_LICENSE', getenv( 'LICENSE_ACF' ) ?: '' );
+    define( 'FACETWP_LICENSE_KEY', getenv( 'LICENSE_FACETWP' ) ?: '' );
     define( 'GF_LICENSE_KEY', getenv( 'LICENSE_GRAVITYFORMS' ) ?: '' );
+	define( 'GPERKS_LICENSE_KEY', getenv( 'LICENSE_GRAVITYPERKS' ) ?: '' );
+    define( 'WPMDB_LICENCE', getenv( 'LICENSE_WP_MIGRATE' ) ?: '' );
     define( 'SHORTPIXEL_API_KEY', getenv( 'LICENSE_SHORTPIXEL' ) ?: '' );
     define( 'WPCOM_API_KEY', getenv( 'LICENSE_AKISMET' ) ?: '' );
-    define( 'ACF_PRO_LICENSE', getenv( 'LICENSE_ACF' ) ?: '' );
     define( 'WP_ROCKET_KEY', getenv( 'LICENSE_WPROCKET' ) ?: '' );
     define( 'WP_ROCKET_EMAIL', getenv( 'LICENSE_WPROCKET_EMAIL' ) ?: '' );
-    define( 'FACETWP_LICENSE_KEY', getenv( 'LICENSE_FACETWP' ) ?: '' );
